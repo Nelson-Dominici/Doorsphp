4ec65676f2acea0d0d6b3decde1d67e7b35d3bca
Nelson-Dominici <nelsondominici777@gmail.com>
2023-02-17 15:38:53 -0300

    🚀 calling function

---
+++ app/Route/CallFunc.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace app\Route;
+
+use app\Handle\Request\Check\CheckMandatoryData;
+use app\Handle\Request\ReqBody;
+use app\Handle\Route\Uri\QueryParams;
+use app\Utils\AppException;
+
+class CallFunc
+{
+	public static function call(){
+	
+		$httpMethod = $_SERVER["REQUEST_METHOD"];
+		$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+		$route = GetRoute::get($uri, $httpMethod);
+		$func = GetFunc::get($route["funcPath"]);
+
+		$req["queryParams"] = QueryParams::get();
+
+		if (array_key_exists("uriParams", $route)) {
+			$req["uriParams"] = $route["uriParams"];
+		}
+
+		if($httpMethod !== "GET" && $httpMethod !== "HEADE"){
+
+			$req["body"] = ReqBody::get();
+
+			if(array_key_exists(0, $route)){
+				CheckMandatoryData::check($req["body"], $route[0]);
+			}
+		}
+
+		call_user_func(array($func["controller"], $func["method"]), $req);
+		
+		exit();
+	}
+	
+}
