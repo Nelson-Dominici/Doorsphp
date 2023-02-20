eec505d88678361614375d2d42e0200707e28467
Nelson-Dominici <nelsondominici777@gmail.com>
2023-02-20 19:20:06 -0300

    try catch, native feature

--- index.php
+++ index.php
@@ -2,13 +2,6 @@
 
 require("bootstrap.php");
 
-use app\Utils\AppException;
-use app\Route\CallFunc;
+use app\NativeFeatures\Route\CallFunc;
 
-try {
-	
-	CallFunc::call();
-	
-} catch (AppException $e) {
-	$e->sendException();
-}
+CallFunc::call();
