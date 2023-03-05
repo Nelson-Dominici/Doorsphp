9d7f067ff323df1d4a6d6933b0b48f25597acc09
Nelson-Dominici <nelsondominici777@gmail.com>
2023-03-05 20:25:56 -0300

    moving places

---
+++ app/NativeResources/Request/DataServices/QueryParams.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace app\NativeResources\Request\DataServices;
+
+class QueryParams
+{
+
+	public static function get(){
+
+		if(!empty($_SERVER['QUERY_STRING'])){
+
+			$query = $_SERVER['QUERY_STRING'];
+
+			$queryParams = []; 
+			
+			foreach(explode("&", $query) as $param) {
+				
+				$keyValue = explode("=", $param);
+
+				if (array_key_exists(1, $keyValue)) {
+					$queryParams["$keyValue[0]"] = "$keyValue[1]";
+
+				} else {
+					$queryParams["$keyValue[0]"] = "";
+				}
+			}
+
+			return $queryParams;
+		};
+
+		return [];
+	}
+
+}
