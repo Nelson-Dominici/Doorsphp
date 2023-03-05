274ce9c9fe9a847a39693c455b7d1491688c0004
Nelson-Dominici <nelsondominici777@gmail.com>
2023-03-05 20:24:53 -0300

    Route, example

--- index.php
+++ index.php
@@ -2,6 +2,30 @@
 
 require("bootstrap.php");
 
-use app\NativeResources\Route\CallFunc;
+// you can delete the example.
 
-CallFunc::call();
+use app\NativeResources\Route;
+
+class ClassExample
+{
+	public static function absoluteRoute($req){
+		var_dump($req);
+	}
+
+	public static function uriParamasRoute($req){
+		var_dump($req);
+	}
+
+	public static function postExample($req){
+		var_dump($req);
+	}
+}
+
+$classExample = new ClassExample();
+
+Route::post("/example/post", [$classExample, "postExample"]);
+
+Route::get("/example/:uuid", [$classExample, "uriParamasRoute"]);
+Route::get("/example/:name/example/:age", [$classExample, "uriParamasRoute"]);
+
+Route::get("/", [$classExample, "uriParamasRoute"]);
