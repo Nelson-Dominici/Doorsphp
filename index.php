768a10c5607bb51a62d377b7a20a2046d95976b0
Nelson-Dominici <nelsondominici777@gmail.com>
2023-03-09 21:42:48 -0300

    new resource

--- index.php
+++ index.php
@@ -8,24 +8,22 @@ use app\NativeResources\Route;
 
 class ClassExample
 {
-	public static function absoluteRoute(array $req){
+	public function absoluteRoute(array $req){
 		var_dump($req);
 	}
 
-	public static function uriParamasRoute(array $req){
+	public function uriParamasRoute(array $req){
 		var_dump($req);
 	}
 
-	public static function postExample(array $req){
+	public function postExample(array $req){
 		var_dump($req);
 	}
 }
 
-$classExample = new ClassExample();
+Route::post("/example/post", [ClassExample::class, "postExample"]);
 
-Route::post("/example/post", [$classExample, "postExample"]);
+Route::get("/example/:uuid", [ClassExample::class, "uriParamasRoute"]);
+Route::get("/example/:name/example/:age", [ClassExample::class, "uriParamasRoute"]);
 
-Route::get("/example/:uuid", [$classExample, "uriParamasRoute"]);
-Route::get("/example/:name/example/:age", [$classExample, "uriParamasRoute"]);
-
-Route::get("/", [$classExample, "uriParamasRoute"]);
+Route::get("/", [ClassExample::class, "uriParamasRoute"]);
