d7b43de44a607d9a2ff277f46751b085b546f74f
Nelson-Dominici <nelsondominici777@gmail.com>
2023-03-29 22:31:58 -0300

    changing position files

---
+++ app/Modules/Route/Services/CallRouteFunc.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace app\Modules\Route\Services;
+
+class CallRouteFunc
+{
+	
+	public static function call(array|callable $funcs, array $objects): void{
+
+		[$req, $res] = $objects;
+		$endFunc = array_pop($funcs);
+		
+		$middlewareData = [];
+
+		foreach ($funcs as $func) {
+
+			if(gettype($func) !== "array"){
+			
+				$data = $func($req, $res);
+
+				if($data){
+					$middlewareData = array_merge($middlewareData, $data);
+				}
+
+			}else{
+
+				[$class, $method] = $func;
+				$data = call_user_func(array($class, $method), $req, $res);
+				
+				if($data){
+					$middlewareData = array_merge($middlewareData, $data);
+				}
+
+			}
+		}
+
+		if($middlewareData){
+
+			foreach ($middlewareData as $key => $value) {
+				$req->$key = $value;
+			}
+		};
+
+		if(gettype($endFunc) !== "array"){
+			$endFunc($req, $res);
+			return;
+		}
+
+		[$class, $method] = $endFunc;
+		call_user_func(array($class, $method), $req, $res);
+
+	}
+
+}
