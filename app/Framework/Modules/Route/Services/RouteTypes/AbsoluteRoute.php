275188bc04a76729b3a0851c47a920bba87941c4
Nelson-Dominici <nelsoncomer777@gmail.com>
2023-10-07 12:16:34 -0300

    New method name, PSR IF.

--- app/Framework/Modules/Route/Services/RouteTypes/AbsoluteRoute.php
+++ app/Framework/Modules/Route/Services/RouteTypes/AbsoluteRoute.php
@@ -4,22 +4,20 @@ namespace app\Framework\Modules\Route\Services\RouteTypes;
 
 class AbsoluteRoute
 {
-
-	public static function get(string $uri, string $route): string|bool{
-			
-		$routeParts = explode("/", $route);
+	public static function check(string $uri, string $routePath): string|bool
+	{		
+		$routeParts = explode("/", $routePath);
 		array_shift($routeParts);
 
 		$paramsArray = array_filter($routeParts, function($field) {
 			return strpos($field, ":") === 0;
 		});
 
-		if(empty($paramsArray) && $route === $uri){
+		if (empty($paramsArray) && $routePath === $uri){ 
 
 			return true;
 		}
 
 		return false;
 	}
-
 }
