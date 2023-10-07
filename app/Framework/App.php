0f40d0bb74bb41df3bb85c998dd9ea8138c3bb6d
Nelson-Dominici <nelsoncomer777@gmail.com>
2023-10-07 11:52:31 -0300

    Using Route Trait

--- app/Framework/App.php
+++ app/Framework/App.php
@@ -5,34 +5,9 @@ namespace app\Framework;
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: *");
 
-class App extends Modules\Route\Route
+use app\Framework\Modules\Route\Route;
+
+class App
 {
-
-	public function get(string $route, array|callable ...$funcs): void{
-
-		if ($_SERVER["REQUEST_METHOD"] === "GET") {
-			$this->check($route, $funcs);
-		}
-	}
-
-	public function post(string $route, array|callable ...$funcs): void{
-	
-		if ($_SERVER["REQUEST_METHOD"] === "POST") {
-			$this->check($route, $funcs);
-		}	
-	}
-
-	public function put(string $route, array|callable ...$funcs): void{
-	
-		if ($_SERVER["REQUEST_METHOD"] === "PUT") {
-			$this->check($route, $funcs);
-		}		
-	}
-
-	public function delete(string $route, array|callable ...$funcs): void{
-	
-		if ($_SERVER["REQUEST_METHOD"] === "DELETE") {
-			$this->check($route, $funcs);
-		}			
-	}
+	use Route;
 }
